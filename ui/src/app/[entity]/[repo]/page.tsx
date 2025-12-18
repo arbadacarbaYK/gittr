6570aba43b0f93574ccc69f80831c886f65d15e8
arbadacarbaYK <info@plebtag.com>
2025-12-19 00:48:33 +0100

    Improve link conversion regex

--- ui/src/app/[entity]/[repo]/page.tsx
+++ ui/src/app/[entity]/[repo]/page.tsx
@@ -7877,8 +7877,10 @@ export default function RepoCodePage({
                         }
                         // CRITICAL: Fix links that have path segments instead of query parameters
                         // If href looks like a gittr.space repo link with path segments, convert to query param format
-                        const gittrPathMatch = href.match(/^(https?:\/\/gittr\.space\/[^\/]+\/[^\/]+)\/(.+)$/);
-                        if (gittrPathMatch && gittrPathMatch[1] && gittrPathMatch[2] && !href.includes('?path=') && !href.includes('?file=') && !href.includes('?branch=')) {
+                        // Match: https://gittr.space/npub.../repo-name/path/to/file
+                        // Convert to: https://gittr.space/npub.../repo-name?path=path%2Fto%2Ffile
+                        const gittrPathMatch = href.match(/^(https?:\/\/gittr\.space\/[^\/]+\/[^\/]+)\/([^?#]+)$/);
+                        if (gittrPathMatch && gittrPathMatch[1] && gittrPathMatch[2] && !href.includes('?path=') && !href.includes('?file=') && !href.includes('?branch=') && !href.includes('api/')) {
                           const baseUrl = gittrPathMatch[1];
                           const pathSegment = gittrPathMatch[2];
                           // Convert path segment to query parameter format
